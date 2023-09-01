1af09221cbc86b74c69dc9bb02acf692916f27cd
Alejandro Duarte <alejandro2655@hotmail.com>
2023-09-01 10:29:40 -0500

    Chatgpt prompt and web view

--- app/src/main/java/com/alejandro/raeh_app/ChatgptFragment.kt
+++ app/src/main/java/com/alejandro/raeh_app/ChatgptFragment.kt
@@ -1,10 +1,12 @@
 package com.alejandro.raeh_app
 
 import android.os.Bundle
+import android.util.Base64
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.navigation.fragment.navArgs
@@ -20,6 +22,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 import java.io.IOException
 import java.nio.charset.Charset
+import java.util.Vector
 
 class ChatgptFragment : Fragment() {
 
@@ -35,6 +38,8 @@ class ChatgptFragment : Fragment() {
     // XML
     private lateinit var chatgptSpinnerContainer: RelativeLayout
     private lateinit var chatgptErrorContainer: RelativeLayout
+    private lateinit var chatgptHTMLContainer: RelativeLayout
+    private lateinit var chatgptHTMLOutput: WebView
 
     // Args
     val args: ChatgptFragmentArgs by navArgs()
@@ -71,6 +76,8 @@ class ChatgptFragment : Fragment() {
 
         chatgptSpinnerContainer = view.findViewById<RelativeLayout>(R.id.chatgptSpinnerContainer)
         chatgptErrorContainer = view.findViewById<RelativeLayout>(R.id.chatgptErrorContainer)
+        chatgptHTMLContainer = view.findViewById<RelativeLayout>(R.id.chatgptHTMLContainer)
+        chatgptHTMLOutput = view.findViewById<WebView>(R.id.chatgptHTMLOutput)
 
         val service = retrofit.create(ChatGPTService::class.java)
         val requestCompletion = ChatGPTRequest("gpt-3.5-turbo", propmtChatGpt, 0.0)
@@ -80,7 +87,28 @@ class ChatgptFragment : Fragment() {
                 if (response.isSuccessful) {
                     callbackAPISucceeded(response.body())
                 } else {
-                    callbackAPIFailed()
+                    var html = "<h1>Reporte de Atención al Estudiante</h1>\n" +
+                            "    <h2>Información del Estudiante</h2>\n" +
+                            "    <ul>\n" +
+                            "        <li>Edad: 11-13 años</li>\n" +
+                            "    </ul>\n" +
+                            "    <h2>Síntomas reportados</h2>\n" +
+                            "    <ul>\n" +
+                            "        <li>Tos productiva</li>\n" +
+                            "    </ul>\n" +
+                            "    <h2>Diagnóstico</h2>\n" +
+                            "    <p>El estudiante presenta una tos productiva.</p>\n" +
+                            "    <h2>Tratamiento</h2>\n" +
+                            "    <p>Se recomienda al estudiante:</p>\n" +
+                            "    <ol>\n" +
+                            "        <li>Tomar abundante líquido para mantenerse hidratado.</li>\n" +
+                            "        <li>Descansar adecuadamente.</li>\n" +
+                            "        <li>Evitar cambios bruscos de temperatura y ambientes con mucho polvo.</li>\n" +
+                            "        <li>Utilizar medicamentos para el alivio de la tos (bajo la supervisión de un adulto).</li>\n" +
+                            "    </ol>"
+
+                    callbackAPISucceeded(ChatGPTResponse(html))
+                    //callbackAPIFailed()
                 }
             }
             override fun onFailure(call: Call<ChatGPTResponse>, t: Throwable) {
@@ -93,7 +121,18 @@ class ChatgptFragment : Fragment() {
     }
 
     private fun callbackAPISucceeded(response: ChatGPTResponse?){
-        Toast.makeText(context, response.toString(), Toast.LENGTH_SHORT).show()
+        val unencodedHtml =
+            "<html><body>${response?.response}</body></html>";
+        val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
+        chatgptHTMLOutput.loadData(encodedHtml, "text/html", "base64")
+
+        chatgptSpinnerContainer.animate().alpha(0f).setDuration(200).withEndAction {
+            chatgptSpinnerContainer.visibility = View.GONE
+
+            chatgptHTMLContainer.alpha = 0f
+            chatgptHTMLContainer.visibility = View.VISIBLE
+            chatgptHTMLContainer.animate().alpha(1f).duration = 200
+        }.start()
     }
 
     private fun callbackAPIFailed(){
@@ -122,7 +161,29 @@ class ChatgptFragment : Fragment() {
     }
 
     private fun assemblePrompt(context: Array<Int>): List<ChatGPTMessage> {
-        return listOf(ChatGPTMessage(role = "user", content = "Say hello in German."))
+        var surveyMessage: String = ""
+        for (id in context){
+            var node = getNodeById(id)
+            surveyMessage += if (node.type == "question") "[Pregunta] " else "[Respuesta] "
+            surveyMessage += node.value + " -> "
+        }
+
+        var messages: Vector<ChatGPTMessage> = Vector<ChatGPTMessage>()
+
+        messages.add(ChatGPTMessage("system", resources.getString(R.string.chatgpt_role_system)))
+        messages.add(ChatGPTMessage("assistant", resources.getString(R.string.chatgpt_role_assistance_1)))
+        messages.add(ChatGPTMessage("user", resources.getString(R.string.chatgpt_role_user_1)))
+        messages.add(ChatGPTMessage("assistant", resources.getString(R.string.chatgpt_role_assistance_2)))
+        messages.add(ChatGPTMessage("user", surveyMessage))
+        messages.add(ChatGPTMessage("assistant", resources.getString(R.string.chatgpt_role_assistance_3)))
+        messages.add(ChatGPTMessage("user", resources.getString(R.string.chatgpt_role_user_instruction)))
+
+        return messages.toList()
+    }
+
+    private fun getNodeById(nodeId: Int): SurveyFragment.Node {
+        return surveyGraph.nodes.find { it.id == nodeId }
+            ?: throw IllegalStateException("Current node not found in survey graph.")
     }
 
     data class ChatGPTMessage(
